094bc3d79c3c82c9cabd8fd055d0ad2ed40cd58e
Lee Goddard <leegee@gmail.com>
2018-09-21 10:37:00 +0200

    Use current Polymer API

--- src/components/app/index.ts
+++ src/components/app/index.ts
@@ -1,4 +1,4 @@
-import { Element as PolymerElement } from '@polymer/polymer/polymer-element';
+import { PolymerElement } from '@polymer/polymer/polymer-element';
 
 import * as view from './app.template.html';
 
